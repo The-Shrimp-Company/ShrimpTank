caf66714fbb9a2eebff3b0b7a3750d94cdb82294
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-07-13 17:39:03 +0100

    new Food interface

---
+++ Assets/Scripts/UI/FoodButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI itemCount;
+    [SerializeField] private TextMeshProUGUI itemName;
+
+    public void AssignFood(Item food, TankController tank)
+    {
+        itemCount.text = Inventory.GetItemQuantity(food).ToString();
+        itemName.text = food.itemName;
+
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (Inventory.HasItem(food))
+            {
+                Inventory.RemoveItem(food);
+                GameObject newFood = Instantiate(((FoodItemSO)Inventory.GetSOForItem(food)).foodPrefab, tank.GetRandomSurfacePosition(), Quaternion.identity);
+                newFood.GetComponent<ShrimpFood>().CreateFood(tank);
+                itemCount.text = Inventory.GetItemQuantity(food).ToString();
+                if(Inventory.GetItemQuantity(food) <= 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        });
+    }
+}
