6f957afacb7cc49fcac1326added5fe934be0659
AJM-03 <a.moreland2003@gmail.com>
2025-08-30 15:54:44 +0100

    Inventory UI Improvements
    - Added tweening to decoration content buttons

--- Assets/Scripts/UI/DecorationContentBlock.cs
+++ Assets/Scripts/UI/DecorationContentBlock.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
-public class DecorationContentBlock : ContentBlock
+public class DecorationContentBlock : ContentBlock, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private ItemSO so;
 
@@ -14,13 +16,38 @@ public class DecorationContentBlock : ContentBlock
     public TMP_Text priceText;
     public TMP_Text ownedText;
 
+    [Header("Colours")]
     public Color inInventoryColour;
     public Color notInInventoryColour;
     public Color cannotAffordColour;
     public Color selectedColour;
 
+    [Header("Scale")]
+    public Vector3 hoverScale;
+    public float hoverScaleDuration;
+
     public void SetDecoration(ItemSO d)
     {
         so = d;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOKill();
+        transform.DOScale(hoverScale, hoverScaleDuration);
+        transform.DOLocalRotate(Vector3.zero, hoverScaleDuration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOKill();
+        transform.DOScale(Vector3.one, hoverScaleDuration);
+        transform.DOLocalRotate(Vector3.zero, hoverScaleDuration);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.DOKill();
+        transform.DOPunchRotation(new Vector3(0, 0, 10), 0.5f);
     }
 }
