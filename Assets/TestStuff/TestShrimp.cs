0af850765abc8fc48fa5bbfe2569e0927318c2cc
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-11-04 17:08:26 +0000

    Reimporting stuff

--- Assets/TestStuff/TestShrimp.cs
+++ Assets/TestStuff/TestShrimp.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class TestShrimp : MonoBehaviour
 {
     public Transform shrimpModel;
+    public TraitSet fullSet;
 
     // Start is called before the first frame update
     void Start()
     {
-        ShrimpStats s = ShrimpManager.instance.CreatePureBreedShrimp(TraitSet.Cherry, true);
+        ShrimpStats s = ShrimpManager.instance.CreatePureBreedShrimp(fullSet, true);
         GameObject newShrimp = Instantiate(GeneManager.instance.GetTraitSO(s.legs.activeGene.ID).part, shrimpModel);
         newShrimp.GetComponent<Legs>().Construct(s);
     }
