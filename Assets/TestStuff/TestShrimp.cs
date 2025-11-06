40fe2a0ee6b65272cf871f59f7df9c6e7e6bc9db
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-11-06 14:59:49 +0000

    Final Animation Push

--- Assets/TestStuff/TestShrimp.cs
+++ Assets/TestStuff/TestShrimp.cs
@@ -6,13 +6,23 @@ public class TestShrimp : MonoBehaviour
 {
     public Transform shrimpModel;
     public TraitSet fullSet;
+    public PartScript.AnimNames anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        ShrimpStats s = ShrimpManager.instance.CreatePureBreedShrimp(fullSet, true);
+        ShrimpStats s;
+        if(fullSet == TraitSet.None)
+        {
+            s = ShrimpManager.instance.CreateRandomShrimp(true);
+        }
+        else
+        {
+            s = ShrimpManager.instance.CreatePureBreedShrimp(fullSet, true);
+        }
         GameObject newShrimp = Instantiate(GeneManager.instance.GetTraitSO(s.legs.activeGene.ID).part, shrimpModel);
         newShrimp.GetComponent<Legs>().Construct(s);
+        newShrimp.GetComponent<Legs>().SetAnimation(anim);
     }
 
     
